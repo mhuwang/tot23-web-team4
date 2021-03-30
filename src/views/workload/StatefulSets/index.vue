d0acd84ec633ea7a2e329309fff7903c013ce377
zqyzz <870995241@qq.com>
2021-03-30 19:39:41 +0800

    2021.3.30 zqy-workload

--- src/views/workload/StatefulSets/index.vue
+++ src/views/workload/StatefulSets/index.vue
@@ -6,13 +6,92 @@
  * @LastEditors: Rex Joush
  * @LastEditTime: 2021-03-19 16:08:23
 -->
-<template>
+<!--<template>
   <h1>Stateful Sets</h1>
+</template>-->
+<template>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>所有 Pod</span>
+      </div>
+      <el-table :data="pods" style="width: 100%" stripe>
+        <el-table-column width="40">
+          <template slot-scope="scope">
+            <svg-icon :icon-class="scope.row.status.conditions[3].status == 'True'? 'load-success': scope.row.status.conditions[3].status == 'Unknown'?'load-doubt':'load-failed'"/>
+          </template>
+        </el-table-column>
+        <el-table-column prop="metadata.name" label="名字">
+          <template slot-scope="scope">
+            <router-link :to="'/workload/pods/'+scope.row.metadata.name" @click.native="goToPodsDetails(scope.row)" class="link-type">
+              <span style="color:#409EFF;text-decoration:underline">{{ scope.row.metadata.name }}</span>
+            </router-link>
+          </template>
+        </el-table-column>
+        <el-table-column prop="metadata.namespace" label="命名空间"> </el-table-column>
+        <!-- <el-table-column label="标签">
+          <template slot-scope="scope">
+            <span>k8s-app: {{scope.row.metadata.labels['k8s-app']}}</span>
+            <br>
+            <span>pod-template-hash: {{scope.row.metadata.labels['pod-template-hash']}}</span>
+          </template>
+        </el-table-column> -->
+        <!-- <el-table-column prop="apiVersion" label="apiVersion"> </el-table-column> -->
+        <!-- <el-table-column prop="kind" label="kind"> </el-table-column> -->
+        <!-- <el-table-column prop="metadata.uid" label="uid"> </el-table-column> -->
+        <el-table-column prop="spec.nodeName" width="140" label="所属节点"> </el-table-column>
+        <el-table-column prop="status.podIP" width="140" label="主机ip地址"> </el-table-column>
+        <el-table-column label="启动时间" width="200">
+          <template slot-scope="scope">
+            <span>{{scope.row.status.startTime.replaceAll(/[TZ]/g,' ')}}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+              <!-- 修改 -->
+              <el-button type="primary" icon="el-icon-plus" size="small" @click="showClasterRolesAddDialog(scope.row)">增加</el-button>
+              <!-- 删除 -->
+              <el-button type="warning" icon="el-icon-edit" size="small" @click="showClasterRolesEditDialog(scope.row)">编辑</el-button>
+              <!-- 删除 -->
+              <el-button type="danger" icon="el-icon-delete" size="small" @click="delClasterRoles(scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Pods",
+
+  data() {
+    return {
+      pods: [],
+    };
+  },
+
+  created() {
+    this.getPods();
+  },
+
+  methods: {
+    // 获取所有 pods
+    getPods() {
+      this.$store
+        .dispatch("pods/getAllPods")
+        .then((res) => {
+          console.log(res.data);
+          this.pods = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
+
 
 <style lang="less" scoped>
 </style>
