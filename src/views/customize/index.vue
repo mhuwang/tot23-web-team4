72193be6fde53ebfcd44b10e30c7fde39401cbd7
bernie-guo <1456252990@qq.com>
2021-04-02 13:00:52 +0800

    2021.4.2-bernie-guo

--- src/views/customize/index.vue
+++ src/views/customize/index.vue
@@ -12,28 +12,61 @@
       <div slot="header" class="clearfix">
         <span>所有 CRD</span>
       </div>
-      <el-table :data="CRDs" style="width: 100%" stripe>
-        <el-table-column width="40">
+    
+       <el-table :data="crd" style="width: 100%" stripe>
+        <!--<el-table-column width="40">
           <template slot-scope="scope">
-            <svg-icon :icon-class="scope.row.status.conditions[3].status == 'True'? 'load-success': scope.row.status.conditions[3].status == 'Unknown'?'load-doubt':'load-failed'"/>
+            <svg-icon :icon-class="scope.row.status.conditions[1].status == 'True'? 'load-success': scope.row.status.conditions[1].status == 'Unknown'?'load-doubt':'load-failed'"/>
+          </template>
+        </el-table-column> -->
+        <el-table-column prop="metadata.name" label="名字">
+          <template slot-scope="scope">
+            <router-link :to="'/customize/'+scope.row.metadata.name" @click.native="goToCRDDetails(scope.row)" class="link-type">
+              <span style="color:#409EFF;text-decoration:underline">{{ scope.row.metadata.name }}</span>
+            </router-link>
           </template>
         </el-table-column>
-        <el-table-column prop="" label="名字"></el-table-column>
-        <el-table-column prop="" label="Group"> </el-table-column>
-        <el-table-column prop="" label="全名"></el-table-column> -->
-        <el-table-column prop="spec.nodeName" width="140" label="Nmaespaceed"> </el-table-column>
-        <el-table-column prop="status.podIP" width="140" label="创建时间"> </el-table-column>
+        <!-- <el-table-column prop="apiVersion" label="版本"> </el-table-column> -->
+        <el-table-column prop="spec.group" label="Group"> </el-table-column>
+        <!-- <el-table-column label="标签">
+          <template slot-scope="scope">
+            <span>k8s-app: {{scope.row.metadata.labels['k8s-app']}}</span>
+            <br>
+            <span>pod-template-hash: {{scope.row.metadata.labels['service-template-hash']}}</span>
+          </template>
+        </el-table-column> -->
+        <!-- <el-table-column prop="apiVersion" label="apiVersion"> </el-table-column> -->
+        <!-- <el-table-column prop="kind" label="kind"> </el-table-column> -->
+        <!-- <el-table-column prop="metadata.uid" label="uid"> </el-table-column> -->
+        <!-- <el-table-column prop="spec.nodeName" width="140" label="所属节点"> </el-table-column>
+        <el-table-column prop="status.podIP" width="140" label="主机ip地址"> </el-table-column> -->
+        <el-table-column prop="spec.scope" label="scope"> </el-table-column>
+        <el-table-column label="创建时间" width="200">
+          <template slot-scope="scope">
+            <span>{{scope.row.metadata.creationTimestamp.replaceAll(/[TZ]/g,' ')}}</span>
+          </template>
+        </el-table-column>
         <el-table-column label="操作">
-        
-        <!-- <template slot-scope="scope">
-            
-            <el-button type="primary" icon="el-icon-plus" size="small" @click="showClasterRolesAddDialog(scope.row)">增加</el-button>
-            
-            <el-button type="warning" icon="el-icon-edit" size="small" @click="showClasterRolesEditDialog(scope.row)">编辑</el-button>
-            
-            <el-button type="danger" icon="el-icon-delete" size="small" @click="delClasterRoles(scope.row)">删除</el-button>
-        </template> -->
-        
+          <template slot-scope="scope">
+            <!-- 修改 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              style="margin-bottom:5px"
+              size="small"
+              @click="showClasterRolesEditDialog(scope.row.pod)"
+              >编辑</el-button
+            >
+            <br>
+            <!-- 删除 -->
+            <el-button
+              type="danger"
+              icon="el-icon-delete"
+              size="small"
+              @click="delClasterRoles(scope.row.pod)"
+              >删除</el-button
+            >
+          </template>
         </el-table-column>
       </el-table>
     </el-card>
@@ -42,26 +75,26 @@
 
 <script>
 export default {
-  name: "CRDs",
+  name: "crd",
 
   data() {
     return {
-      CRDs: [],
+      crd: [],
     };
   },
 
   created() {
-    this.getCRDs();
+    this.getcrd();
   },
 
   methods: {
     // 获取所有 CRD
-    getCRDs() {
+    getcrd() {
       this.$store
         .dispatch("customize/getCustomResourceDefinition")
         .then((res) => {
           console.log(res.data);
-          this.getCRDs = res.data;
+          this.crd = res.data;
         })
         .catch((error) => {
           console.log(error);
