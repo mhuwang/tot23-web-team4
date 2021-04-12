0c35369fe442b564d74b53de0c80f7cfd949a15e
bernie-guo <1456252990@qq.com>
2021-04-12 19:25:44 +0800

    2021.4.12

--- src/api/common.js
+++ src/api/common.js
@@ -3,13 +3,14 @@
  * @version: 1.0
  * @Author: Rex Joush
  * @Date: 2021-04-07 20:10:50
- * @LastEditors: zqy
- * @LastEditTime: 2021-04-11 21:04:55
+ * @LastEditors: Bernie
+ * @LastEditTime: 2021-04-12 12:30:30
  */
 import request from '@/utils/request'
 let baseUrl = "http://localhost:8081"
 
 export function changeResourceByYaml(data) {
+  console.log("yaml",data);
     return request({
       url: baseUrl + '/common/changeResourceByYaml',
       method: 'post',
