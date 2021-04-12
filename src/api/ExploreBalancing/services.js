a8d25cb0be6b2ac06b6f2a9b847bc11b2e86e6bb
LeoYoung1996 <LeoYoung1996>
2021-04-12 10:54:17 +0800

    2021.04.12 Leo

--- src/api/ExploreBalancing/services.js
+++ src/api/ExploreBalancing/services.js
@@ -3,8 +3,8 @@
  * @version: 1.0
  * @Author: Rex Joush
  * @Date: 2021-03-27 14:23:44
- * @LastEditors: Rex Joush
- * @LastEditTime: 2021-03-27 14:24:21
+ * @LastEditors: Leo
+ * @LastEditTime: 2021-04-12 10:49:28
  */
 import request from '@/utils/request'
 let baseUrl = "http://localhost:8081"
@@ -13,5 +13,63 @@ export function getAllServices(data) {
   return request({
     url: baseUrl + '/service/getAllServices',
     method: 'get',
+    params: {
+      namespace: data
+    }
   })
 }
+
+export function getServicesByNameAndNamespace(svc) {
+  return request({
+    url: baseUrl + '/service/getServiceByNameAndNamespace',
+    method: 'get',
+    params: {
+      name: svc.name,
+      namespace: svc.namespace
+    }
+  })
+}
+
+export function getServiceYamlByNameAndNamespace(data) {
+  return request({
+    url: baseUrl + '/service/getServiceYamlByNameAndNamespace',
+    method: 'get',
+    params: {
+      name: data.serviceName,
+      namespace: data.serviceNamespace,
+    }
+  })
+}
+
+export function delServiceByNameAndNamespace(data) {
+  return request({
+    url: baseUrl + '/service/delServiceByNameAndNamespace',
+    method: 'get',
+    params: {
+      name: data.serviceName,
+      namespace: data.serviceNamespace,
+    }
+  })
+}
+
+export function getEndpointBySvcNameAndNamespace(svc) {
+  return request({
+    url: baseUrl + '/service/getEndpointBySvcNameAndNamespace',
+    method: 'get',
+    params: {
+      name: svc.name,
+      namespace: svc.namespace
+    }
+  })
+}
+
+// export function getPodBySvcLabel(lab) {
+//   return request({
+//     url: baseUrl + '/service/getPodBySvcLabel',
+//     method: 'get',
+//     params: {
+//       key: lab.name,
+//       value: lab.namespace
+//     }
+//   })
+// }
--- src/store/modules/ExploreBalancing/services.js
+++ src/store/modules/ExploreBalancing/services.js
@@ -3,10 +3,10 @@
  * @version: 1.0
  * @Author: Rex Joush
  * @Date: 2021-03-27 14:18:28
- * @LastEditors: Rex Joush
- * @LastEditTime: 2021-03-27 14:39:08
+ * @LastEditors: Leo
+ * @LastEditTime: 2021-04-11 21:41:41
  */
-import { getAllServices } from '@/api/ExploreBalancing/services'
+import { getAllServices, getServicesByNameAndNamespace, getServiceYamlByNameAndNamespace, delServiceByNameAndNamespace } from '@/api/ExploreBalancing/services'
 import { getToken } from '@/utils/auth'
 
 const state = {
@@ -19,7 +19,7 @@ const state = {
 
 const mutations = {
     // 跳转 service 详情页面
-    TO_SERVICES_DETIALS: (state, {serviceName, serviceNamespace}) => {
+    TO_SERVICES_DETIALS: (state, { serviceName, serviceNamespace }) => {
         // 赋值
         state.service.serviceName = serviceName;
         state.service.serviceNamespace = serviceNamespace;
@@ -28,9 +28,9 @@ const mutations = {
 
 const actions = {
     // getAllServices
-    getAllServices({ commit }) {
+    getAllServices({ commit }, namespace) {
         return new Promise((resolve, reject) => {
-            getAllServices().then(response => {
+            getAllServices(namespace).then(response => {
                 const { data } = response
                 if (!data) {
                     return reject('获取失败')
@@ -57,8 +57,40 @@ const actions = {
         })
     },
 
-     // 点击名字进入详情页
-     toDetails({ commit }, dep) {
+
+    // 通过名称和命名空间获取对应的 service yaml 文件
+    getServiceYamlByNameAndNamespace({ commit }, serviceDetails) {
+
+        return new Promise((resolve, reject) => {
+            getServiceYamlByNameAndNamespace(serviceDetails).then(response => {
+                const { data } = response
+                if (!data) {
+                    return reject('获取失败')
+                }
+                resolve(data)
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    },
+
+    //通过名称和命名空间删除一个service
+    delServiceByNameAndNamespace({ commit }, serviceDetails) {
+        return new Promise((resolve, reject) => {
+            delServiceByNameAndNamespace(serviceDetails).then(response => {
+                const { data } = response
+                if (!data) {
+                    return reject('获取失败')
+                }
+                resolve(data)
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    },
+
+    // 点击名字进入详情页
+    toDetails({ commit }, dep) {
         commit("TO_SERVICES_DETIALS", dep);
     },
 }
